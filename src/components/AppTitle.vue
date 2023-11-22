46a9cc704f07840a84d0798290adea7f7bc651c3
simonemassenti <s.massenti96@gmail.com>
2023-11-22 16:41:53 +0100

    add: new app with Vite

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+    export default {
+        data () {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style>
+</style>
